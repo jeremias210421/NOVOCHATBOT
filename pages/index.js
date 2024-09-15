b04c800af257f2bb33793444de3c95d1ce85bac0
jeremias210421 <jeremias210421@gmail.com>
2024-09-14 22:45:17 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import WhatsAppChatbot from '../components/WhatsAppChatbot'
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <WhatsAppChatbot />
+    </div>
+  )
+}
